bf6c0cbc5cede376caf7fac6da8b8d91c0d5f0bc
Selion11 <gdecaro@itba.edu.ar>
2026-02-01 22:38:06 +0100

    chore(api): Added missing endpoints to express

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -7,5 +7,9 @@ const companyUploads = require('../middlewares/upload.middleware');
 router.post('/create',companyUploads, companyController.createCompany);
 router.get('/:cuit/documents',companyController.listDocuments);
 router.patch('/:cuit/documents',companyUploads, companyController.updateSingleDocument);
+router.get('/:cuit/risk-score',companyController.getRiskScore);
+router.get('/:cuit', companyController.getCompanyDetail);
+router.patch('/:cuit/status', companyController.updateStatus);
+router.get('/', companyController.listCompanies);
 
 module.exports = router;
