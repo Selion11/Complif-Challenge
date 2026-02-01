ae0fe7d37f5c0277fdd8f9c66b5d8b838be2b6dc
Selion11 <gdecaro@itba.edu.ar>
2026-02-01 22:36:30 +0100

    chore(server): Optimized server startup to init tables

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 require('dotenv').config(); 
 
+const sequelize = require('./config/database');
+const Company = require('./models/company.model');
+const logger = require('./utils/logger');
+
 const app = require('./app'); 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${port}`);
-  console.log(`🌍 Entorno: ${process.env.NODE_ENV}`);
+  logger.info({ 
+    service: 'System', 
+    message: `Servidor corriendo en el puerto ${port} en entorno ${process.env.NODE_ENV}` 
+  });
 });
+
+sequelize.sync({ alter: true }) // 'alter' actualiza las tablas si cambias el modelo
+  .then(() => {
+    logger.info({ service: 'Database', message: 'Tablas sincronizadas correctamente en PostgreSQL' });
+  })
+  .catch((err) => {
+    logger.error({ service: 'Database', message: 'Error sincronizando tablas', error: err.message });
+  });
